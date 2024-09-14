a403055773ec4ac67b7a56d8261d7578c130ea36
jvliouc <julio.orozco@uc.cl>
2024-09-14 00:39:37 -0300

    codigo casi completo, solo faltan detalles

--- Tarea 1.R
+++ Tarea 1.R
@@ -1,7 +1,181 @@
 #------------------------------------------------------------------------------#
 library(readxl)
-cpi <- read_excel("tarea1TEFI/cpi_2015.xlsx")
-data <- read_excel("tarea1TEFI/data_tarea_1.xlsx")
+library(dplyr)
+library(ggplot2)
+
+dt_cpi <- read_excel("tarea1TEFI/cpi_2015.xlsx")
+dt  <- read_excel("tarea1TEFI/data_tarea_1.xlsx")
 #------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------#
 #PARTE 1
+#primero vamos a seleccionar las siguientes acciones por medio de su cusip
+#------------------------------------------------------------------------------#
+stock1<-"01623010"#alico
+stock2<-"02358610"#Amerco
+stock3<-"07745410"#BELDEN
+#------------------------------------------------------------------------------#
+dt_s1<- filter(dt, cusip == stock1)
+dt_s2<- filter(dt, cusip == stock2)
+dt_s3<- filter(dt, cusip == stock3)
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#PARTE 2 EN EL INFORME
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#PARTE 3
+#primero se pide calcular retornos simples considerando dividendos
+#para ello se va a usar la fórmula (1+R_t)=(p_t+div_t)/p_t-1
+# en la base de datos ya tengo div_t y p_t, tengo que crear una columna con los p_t-1
+#------------------------------------------------------------------------------#
+dt_s1$p_t_1<-dt_s1$prc
+dt_s1<-dt_s1 %>%
+  mutate(p_t_1 = ifelse(datevar>431,lag(prc, n = 1),prc)) %>%
+  mutate(r_bruto=(prc+divamt)/p_t_1)%>%
+  mutate(r_neto=r_bruto-1)
+tab_s1_3 <- dt_s1 %>%
+  summarise(
+    Media = mean(r_neto),
+    Error_Estandar = sd(r_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_neto),
+    Minimo = min(r_neto),
+    Maximo = max(r_neto),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s2$p_t_1<-dt_s2$prc
+dt_s2<-dt_s2 %>%
+  mutate(p_t_1 = ifelse(datevar>431,lag(prc, n = 1),prc)) %>%
+  mutate(r_bruto=(prc+divamt)/p_t_1)%>%
+  mutate(r_neto=r_bruto-1)
+tab_s2_3 <- dt_s2 %>%
+  summarise(
+    Media = mean(r_neto),
+    Error_Estandar = sd(r_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_neto),
+    Minimo = min(r_neto),
+    Maximo = max(r_neto),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s3$p_t_1<-dt_s3$prc
+dt_s3<-dt_s3 %>%
+  mutate(p_t_1 = ifelse(datevar>431,lag(prc, n = 1),prc)) %>%
+  mutate(r_bruto=(prc+divamt)/p_t_1)%>%
+  mutate(r_neto=r_bruto-1)
+tab_s3_3 <- dt_s3 %>%
+  summarise(
+    Media = mean(r_neto),
+    Error_Estandar = sd(r_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_neto),
+    Minimo = min(r_neto),
+    Maximo = max(r_neto),
+    Numero_Datos = n()
+  )
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#PARTE 4
+#Realizar test de hipotesis media 0
+t_test_s1 <- t.test(dt_s1$r_neto, mu = 0)
+t_test_s2 <- t.test(dt_s2$r_neto, mu = 0)
+t_test_s3 <- t.test(dt_s3$r_neto, mu = 0)
+test_hipotesis<-data.frame(
+  valor_p=c(t_test_s1$p.value,t_test_s2$p.value,t_test_s3$p.value)
+  )
+test_hipotesis<-test_hipotesis%>%
+  mutate(alfa_10 = ifelse(valor_p<=0.1,"Se rechaza H0 (media=0) con 10% de significancia","No se rechaza H0"))%>%
+  mutate(alfa_05 = ifelse(valor_p<=0.05,"Se rechaza H0 (media=0) con 5% de significancia","No se rechaza H0"))%>%
+  mutate(alfa_01 = ifelse(valor_p<=0.01,"Se rechaza H0 (media=0) con 1% de significancia","No se rechaza H0"))
+
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#PARTE 5
+#calculamos el retorno logaritmico como r_t=log(1+Rt), es decir logaritmo del 
+#retorno bruto
+dt_s1<-dt_s1 %>%
+  mutate(r_t = log(r_bruto))
+tab_s1_log_5 <- dt_s1 %>%
+  summarise(
+    Media = mean(r_t),
+    Error_Estandar = sd(r_t)/sqrt(n()),
+    Desviacion_Estandar = sd(r_t),
+    Minimo = min(r_t),
+    Maximo = max(r_t),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s2<-dt_s2 %>%
+  mutate(r_t = log(r_bruto))
+tab_s2_log_5 <- dt_s2 %>%
+  summarise(
+    Media = mean(r_t),
+    Error_Estandar = sd(r_t)/sqrt(n()),
+    Desviacion_Estandar = sd(r_t),
+    Minimo = min(r_t),
+    Maximo = max(r_t),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s3<-dt_s3 %>%
+  mutate(r_t = log(r_bruto))
+tab_s3_log_5 <- dt_s3 %>%
+  summarise(
+    Media = mean(r_t),
+    Error_Estandar = sd(r_t)/sqrt(n()),
+    Desviacion_Estandar = sd(r_t),
+    Minimo = min(r_t),
+    Maximo = max(r_t),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+#PARTE 6
+#primero calculamos la inflacion bruta mensual
+dt_cpi<-dt_cpi %>%
+  mutate(ipc_t_1 = ifelse(year>1995,lag(cpi, n = 1),cpi)) %>%
+  mutate(inflacion_bruta=cpi/ipc_t_1)
+#luego agregamos a la base de datos de cada accion y calculamos el retorno bruto real y neto real
+dt_s1<-dt_s1 %>%
+  mutate(inflacion_bruta=dt_cpi$inflacion_bruta)%>%
+  mutate(r_real_bruto=r_bruto/inflacion_bruta)%>%
+  mutate(r_real_neto=r_real_bruto-1)
+tab_s1_real <- dt_s1 %>%
+  summarise(
+    Media = mean(r_real_neto),
+    Error_Estandar = sd(r_real_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_real_neto),
+    Minimo = min(r_real_neto),
+    Maximo = max(r_real_neto),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s2<-dt_s2 %>%
+  mutate(inflacion_bruta=dt_cpi$inflacion_bruta)%>%
+  mutate(r_real_bruto=r_bruto/inflacion_bruta)%>%
+  mutate(r_real_neto=r_real_bruto-1)
+tab_s2_real <- dt_s2 %>%
+  summarise(
+    Media = mean(r_real_neto),
+    Error_Estandar = sd(r_real_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_real_neto),
+    Minimo = min(r_real_neto),
+    Maximo = max(r_real_neto),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+dt_s3<-dt_s3 %>%
+  mutate(inflacion_bruta=dt_cpi$inflacion_bruta)%>%
+  mutate(r_real_bruto=r_bruto/inflacion_bruta)%>%
+  mutate(r_real_neto=r_real_bruto-1)
+tab_s2_real <- dt_s3 %>%
+  summarise(
+    Media = mean(r_real_neto),
+    Error_Estandar = sd(r_real_neto)/sqrt(n()),
+    Desviacion_Estandar = sd(r_real_neto),
+    Minimo = min(r_real_neto),
+    Maximo = max(r_real_neto),
+    Numero_Datos = n()
+  )
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
